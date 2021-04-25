83a7211b993de402ec55b76df5d16af95de87db7
Tyler Johnson <tyler@csdconsultants.us>
2021-04-24 22:35:03 -0700

    debugging code. have app running with frontend and caching

--- modules/weather.js
+++ modules/weather.js
@@ -2,16 +2,17 @@
 
 let cache = require('./cache.js');
 
-module.exports = getWeather;
+const superagent = require('superagent');
 
-function getWeather(latitude, longitude) {
+
+const getWeather = (latitude, longitude) => {
   const key = 'weather-' + latitude + longitude;
   const url = 'http://api.weatherbit.io/v2.0/forecast/daily';
   const queryParams = {
-    key: WEATHER_API_KEY,
+    key: process.env.WEATHER_API_KEY,
     lang: 'en',
-    lat: lat,
-    lon: lon,
+    lat: latitude,
+    lon: longitude,
     days: 5,
   };
 
@@ -21,12 +22,12 @@ function getWeather(latitude, longitude) {
     console.log('Cache miss');
     cache[key] = {};
     cache[key].timestamp = Date.now();
-    cache[key].data = superagent.get(url)
-    .then(response => parseWeather(response.body));
+    cache[key].data = superagent.get(url).query(queryParams)
+      .then(response => parseWeather(response.body));
   }
-  
+
   return cache[key].data;
-}
+};
 
 function parseWeather(weatherData) {
   try {
@@ -41,7 +42,9 @@ function parseWeather(weatherData) {
 
 class Weather {
   constructor(day) {
-    this.forecast = day.weather.description;
-    this.time = day.datetime;
+    this.description = day.weather.description;
+    this.date = day.datetime;
   }
 }
+
+module.exports = getWeather;
